9621da92d76b6f26c24b28b82de2f400dbc42bc8
jrbeverly <jrbeverly@live.com>
2017-04-15 20:10:08 -0400

    Renaming manual to index, so it produces index.html

--- build/build.sh
+++ build/build.sh
@@ -6,4 +6,5 @@ apk --no-cache add curl zip
 mkdir target/ && cd target/
 curl -o artifacts.zip --header "PRIVATE-TOKEN: ${ARTIFACT_TOKEN}" "${GITLAB_URL}/api/v3/projects/${PROJECT_ID}/builds/artifacts/master/download?job=compile_pdf"
 unzip artifacts.zip
-pdf2htmlEX --zoom 1.5 --embed cfijo --dest-dir ../public manual.pdf
+mv manual.pdf index.pdf
+pdf2htmlEX --zoom 1.5 --embed cfijo --dest-dir ../public index.pdf
