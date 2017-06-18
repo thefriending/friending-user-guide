bca45ba1695995866f2cbe61c86611f3b7f3266f
jrbeverly <jrbeverly@live.com>
2017-06-18 18:53:58 +0000

    Remove container after doc generation

--- doc/quick-gen.sh
+++ doc/quick-gen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p images/
-docker run -v $(pwd):/media/ jrbeverly/rsvg:baseimage rsvg-iconset -f icon.svg -o images/icon/
+docker run --rm -v $(pwd):/media/ jrbeverly/rsvg:baseimage rsvg-iconset -f icon.svg -o images/icon/
